81b76179348b2529e1eb0cea4618e6550bcc9228
winston20000511 <winston20000511@gmail.com>
2024-12-13 12:37:53 +0800

    2024/12/13 更新

--- src/main/java/Bean/formComplationBean.java
+++ src/main/java/Bean/formComplationBean.java
@@ -1,9 +1,6 @@
 package Bean;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
